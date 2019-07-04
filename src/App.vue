c47e711ee99f6467258e92dd0be8d514e267e6db
Roberto Viquez <roberto.viquez@asolvi.com>
2019-07-04 16:14:20 +0200

    login works

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,44 @@
 <template>
   <v-app>
     <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
+      <v-toolbar-title class="headline text-uppercase" color="primary">
         <span>Evatic</span>
         <span class="font-weight-light"> Service Hub</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
+      <Login/>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view/>
     </v-content>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import Login from './components/Login'
+import Error from "./components/Error";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Login,
+    Error
   },
   data () {
     return {
       //
     }
-  }
+  },
+  async created() {
+    try {
+      await this.$auth.renewTokens();
+    } catch {
+      // Supress the 'not logged in' error as we can illegitimately get that
+      // when processing the callback url
+    }
+  } 
 }
 </script>
--- src/router.js
+++ src/router.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Profile from './views/Profile.vue'
+import Callback from './components/Callback.vue'
+import auth from './auth/authService'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -14,12 +17,24 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/profile',
+      name: 'profile',
+      component: Profile
+    },
+    {
+      path: '/callback',
+      name: 'callback',
+      component: Callback
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/' || to.path === '/callback' || auth.isAuthenticated()) {
+    return next()
+  }
+
+  auth.login({ target: to.path })
+})
+
+export default router
